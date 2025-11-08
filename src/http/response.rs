df49570cafc7e15c66322672c84615d343974a74
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-11-07 20:04:06 -0700

    ErrorPage enum and fixed html body creation
    Made a new enum to make things less hardcoded.
    
    Fixed the error of passing in the file path as an html page
    
    stopped logging errors of 404 and 403 since those shouldnt be considered errors anymore

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,12 +1,36 @@
+use crate::io;
 use std::io::{ErrorKind, Write};
 use std::net::TcpStream;
-use crate::io;
+
+enum ErrorPage {
+    NotFound,
+    PermissionDenied,
+    InternalServerError,
+}
+
+impl ErrorPage {
+    fn path(&self) -> String {
+        match self {
+            ErrorPage::NotFound => String::from("public/404.html"),
+            ErrorPage::PermissionDenied => String::from("public/403.html"),
+            ErrorPage::InternalServerError => String::from("public/500.html"),
+        }
+    }
+
+    fn status(&self) -> String {
+        match self {
+            ErrorPage::NotFound => String::from("HTTP/1.1 404 NOT FOUND"),
+            ErrorPage::PermissionDenied => String::from("HTTP/1.1 403 PERMISSION DENIED"),
+            ErrorPage::InternalServerError => String::from("HTTP/1.1 500 INTERNAL SERVER ERROR"),
+        }
+    }
+}
 
 /// Handles various HTML pages
 ///
 /// # Example
 ///
-/// HTML 200-page
+/// HTML 200-pages
 /// HTML 403-page permission denied
 /// HTML 404-page not found
 /// HTML 500-page internal server error
@@ -20,21 +44,29 @@ pub fn http_handler(stream: &TcpStream, response: String) {
         Err(e) => {
             match e.kind() {
                 ErrorKind::NotFound => {
-                    eprintln!("Error file not found {}: {}", filename, e);
-                    let (status, filename) = status_filename(String::from("GET /404 HTTP/1.1"));
-                    write_response(&stream, status, &filename);
+                    // Should never error when reading the file here, because we pass in a hardcoded error path
+                    let html_page = io::file::read_file(&ErrorPage::NotFound.path()).expect("_");
+                    write_response(&stream, &ErrorPage::NotFound.status(), &html_page);
                     return;
                 }
                 ErrorKind::PermissionDenied => {
-                    eprintln!("Error permission denied{}: {}", filename, e);
-                    let (status, filename) = status_filename(String::from("GET /403 HTTP/1.1"));
-                    write_response(&stream, status, &filename);
+                    // Should never error when reading the file here, because we pass in a hardcoded error path
+                    let html_page =
+                        io::file::read_file(&ErrorPage::PermissionDenied.path()).expect("_");
+                    write_response(&stream, &ErrorPage::PermissionDenied.status(), &html_page);
                     return;
                 }
                 _ => {
+                    // Something else happened and we should log it
                     eprintln!("Internal server error {}: {}", filename, e);
-                    let (status, filename) = status_filename(String::from("GET /500 HTTP/1.1"));
-                    write_response(&stream, status, &filename);
+                    // Should never error when reading the file here, because we pass in a hardcoded error path
+                    let html_page =
+                        io::file::read_file(&ErrorPage::InternalServerError.path()).expect("_");
+                    write_response(
+                        &stream,
+                        &ErrorPage::InternalServerError.status(),
+                        &html_page,
+                    );
                     return;
                 }
             }
@@ -44,31 +76,38 @@ pub fn http_handler(stream: &TcpStream, response: String) {
     write_response(&stream, &status, &html_page);
 }
 
-fn status_filename<'a>(response: String) -> (&'a str, String) {
+fn status_filename(response: String) -> (String, String) {
     let parts: Vec<&str> = response.split_whitespace().collect();
     if parts.len() < 2 {
-        return ("HTTP/1.1 404", String::from("public/404.html"));
+        return (
+            String::from("HTTP/1.1 404"),
+            String::from("public/404.html"),
+        );
     }
 
     let path = parts[1];
 
     if path.contains("..") {
-        ("HTTP/1.1 404", String::from("public/404.html"))
+        (
+            String::from("HTTP/1.1 404"),
+            String::from("public/404.html"),
+        )
     } else if path == "/" {
-        ("HTTP/1.1 200 OK", String::from("public/welcome.html"))
+        (
+            String::from("HTTP/1.1 200 OK"),
+            String::from("public/welcome.html"),
+        )
     } else {
         let mut path = String::from(path);
         path.push_str(".html");
-        ("HTTP/1.1 200 OK", format!("public{}", path))
+        (String::from("HTTP/1.1 200 OK"), format!("public{}", path))
     }
-
 }
 
-fn write_response(mut stream: &TcpStream, status: &str, html_page: &str) {
+fn write_response(mut stream: &TcpStream, status: &String, html_page: &String) {
     let length = html_page.len();
 
-    let response =
-        format!("{status}\r\nContent-Length: {length}\r\n\r\n{html_page}");
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{html_page}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
